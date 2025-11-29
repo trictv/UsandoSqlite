4e54530bd4ff89598e6842a81702a20719fd64bd
Patrick <rs108954@gmail.com>
2025-11-29 09:02:12 -0300

    projeto

---
+++ app/src/main/java/com/trictv/usandosqlite/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.trictv.usandosqlite
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.trictv.usandosqlite.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun btIncluirOnClick(view: View) {}
+    fun btAlterarOnClick(view: View) {}
+    fun btExcluirOnClick(view: View) {}
+    fun btPesquisarOnClick(view: View) {}
+    fun btListarOnClick(view: View) {}
+}
